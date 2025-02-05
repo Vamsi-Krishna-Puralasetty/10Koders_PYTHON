c55694f2e55f43073f512455fa01f90cb6778f06
Vamsi-Krishna-Puralasetty <puralasettyv@gmail.com>
2025-02-05 23:02:39 +0530

    while loop

---
+++ tasks/5-2-25.py
@@ -0,0 +1,17 @@
+# 1. print 1 to 100 numbers using while loop
+# 2. print 1 to 100 in reverse order using for Loop and while loop
+# 3. reverse a number using while loop without string conversion
+# 	i/p : 1234
+# 	o/p : 4321
+
+# question - 1
+num=1
+while num<101:
+    print(num)
+    num+=1
+
+
+
+
+
+
